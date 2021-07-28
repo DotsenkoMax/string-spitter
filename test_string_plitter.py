e4cf370f91f48de9a1029ce49b62ebb2f8d04192
Dotsenko Max <raxfaxmaxgmail.com>
2021-07-28 10:06:48 +0100

    third test with comma added

--- test_string_plitter.py
+++ test_string_plitter.py
@@ -1,5 +1,6 @@
 from string_plitter import TagManipulator
 
+
 def test_split_empty_string_result_empty_array():
     # arrange
     stringToSplit = ""
@@ -20,6 +21,20 @@ def test_split_string_with_comma_result_empty_array():
     stringToSplit = ","
     regex = ""
     expResult = []
+    result = None
+    cut = TagManipulator()
+
+    # act
+    result = cut.parse_string(stringToSplit, regex)
+
+    # assert
+    assert result == expResult
+
+def test_split_string_with_comma_result_empty_array():
+    # arrange
+    stringToSplit = "java"
+    regex = ""
+    expResult = ["java"]
     result = None
     cut = TagManipulator()
 
