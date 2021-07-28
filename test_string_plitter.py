adb6bbb6e03b9cc4f2ed3f5e40763ae01266c1f3
Dotsenko Max <raxfaxmaxgmail.com>
2021-07-28 10:05:43 +0100

    second test with comma added

--- test_string_plitter.py
+++ test_string_plitter.py
@@ -13,3 +13,18 @@ def test_split_empty_string_result_empty_array():
 
     # assert
     assert result == expResult
+
+
+def test_split_string_with_comma_result_empty_array():
+    # arrange
+    stringToSplit = ","
+    regex = ""
+    expResult = []
+    result = None
+    cut = TagManipulator()
+
+    # act
+    result = cut.parse_string(stringToSplit, regex)
+
+    # assert
+    assert result == expResult
